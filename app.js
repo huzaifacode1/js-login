f996cb75c4e7548926549fd65666190d13e24cdb
unknown <fawadzahid98@gmail.com>
2024-05-29 18:20:53 +0500

    js login

---
+++ app.js
@@ -0,0 +1,13 @@
+var useremail = document.getElementById('email').value
+var userpassword = document.getElementById('password').value
+var login = document.getElementById('btn')
+login.addEventListener('click',()=>{
+    var useremail = document.getElementById('email').value
+var userpassword = document.getElementById('password').value
+if(useremail == 'explain@gmail.com' && userpassword == 12345){
+ alert('welcome')
+}
+else{
+   alert('try agin')
+}
+})
