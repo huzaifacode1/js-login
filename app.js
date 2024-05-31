67b8ece4d28045c4973d951cd751b31a43dfbd61
unknown <fawadzahid98@gmail.com>
2024-05-31 16:41:16 +0500

    js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ login.addEventListener('click',()=>{
 var userpassword = document.getElementById('password').value
 if(useremail == 'explain@gmail.com' && userpassword == 12345){
  alert('welcome')
+ window.location ='index.html'
 }
 else{
    alert('try agin')
